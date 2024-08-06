90c4bb68d5767e04d10c3acc93981a511a1eb201
ajay2512 <bhakarajay033@gmail.com>
2024-08-06 23:30:49 +0530

    added wallet

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import Team from "./pages/Team";
 import Home from "./pages/home/Home";
 import './App.css'
 import Navbar from "./components/Navbar/Navbar";
+ import { ConnectWallet } from "@thirdweb-dev/react";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+      <ConnectWallet />
+
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/team" element={<Team />} />
         <Route path="*" element={<div>Not Found</div>} />
         <Route path="/home" element={<Home/>}/>
+         {/* <Route path='/test' element = {<ConnectWalletComponent />}/>  */}
       </Routes>
+
     </BrowserRouter>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App';
+import './index.css';
+//import { ConnectWallet } from '@thirdweb-dev/react';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThirdwebProvider>
+      <App />
+    </ThirdwebProvider>
+  
+  </React.StrictMode>
+);
